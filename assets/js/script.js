23f72e87d20958178bdc200b6ebdc19ae3a93557
Anupama Dhir <sabhssabhs@yahoo.com>
2016-08-14 05:35:22 +0800

    initial profile files

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$(function() {
+
+    'use strict';
+
+    $("#new-profile").click(function() {
+        $.ajax({
+            // where the data live
+            url: 'https://myexpressapi.herokuapp.com/users/anu',
+            type: 'GET',
+            // data: {cat: 'movies'},
+            dataType: 'json',
+            // beforeSend: function(xhr) {
+            //   xhr.setRequestHeader("X-Mashape-Authorization", "it4o1uG5pxmshtMBdkNgvx66OWJYp1pdLGxjsndxwVOLShk5Oe");
+            // }
+          }).done(successFunction)
+            .fail(failFunction);
+      });
+
+
+      //success function
+      function successFunction(data, dataType, status) {
+        $("#name").text(data.name);
+        $("#age").text(data.age);
+        $("#email").text(data.email);
+        $("#image").text(data.image);
+        $("#quote").text(data.quote);
+      };
+
+
+      // fail function
+      function failFunction(request, textStatus, errorThrown) {
+          alert('An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown);
+      };
+    });
